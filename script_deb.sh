bc88b36c3d72736bf90d90cf184b1a73eea85003
djenko-it <djenko-it@protonmail.com>
2022-11-21 22:26:51 +0100

    correction script deb

--- script_deb.sh
+++ script_deb.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get update
-sudo -y apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -11,6 +11,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-  sudo apt-get update
+  sudo apt-get -y update
 
-  sudo -y apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
